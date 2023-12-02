e788a9f4f615c6dc0a7eb04e948eb3028817e914
Josh Bennett <jbennett@anduril.com>
2023-12-01 16:16:49 -0800

    Day 1 part 1

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# aoc2023
-Advent of Code 2023 built to MISRA C compliance.
+# Advent Of Code 2023
+Santa's getting a healthy dose of MISRA C compliance this year.
+
+## How to Build
+
+#### 1. Navigate into the source directory, create build folder and run `CMake`:
+
+```sh
+mkdir build
+cd build
+cmake [-G generator] ..
+```
+
+  * The `generator` option is the build system you'd like to use. Run `cmake` without arguments to see a full list of available generators.
+    * On Windows, you might use "Visual Studio 12 2013" (VS 2013 32-bits), or "Visual Studio 14 2015 Win64" (VS 2015 64-bits).
+    * On OS X, you might use "Xcode".
+    * On a UNIX-like system, omit the option and a Makefile will be generated.
+
+#### 2. Build it!
+  * The command you'll need to run depends on the generator you chose earlier.
---
+++ day1/part1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+// To run build the code and then do the following command:
+// ```
+// cat ../day1/in.txt | day1/day1part1
+// ```
+
+int main()
+{
+  size_t len = 0;
+  ssize_t lineSize = 0;
+  int allSum = 0;
+  while (true)
+  {
+    char *line = NULL;
+    lineSize = getline(&line, &len, stdin);
+    if (lineSize == -1)
+    {
+      break;
+    }
+    char firstDigit;
+    char lastDigit;
+    int i;
+    bool foundFirstDigit;
+    foundFirstDigit = false;
+    for (i = 0; i < lineSize; ++i)
+    {
+      if (isdigit(line[i]))
+      {
+        if (!foundFirstDigit)
+        {
+          firstDigit = line[i];
+          foundFirstDigit = true;
+        }
+        lastDigit = line[i];
+      }
+    }
+    char firstLast[2];
+    firstLast[0] = firstDigit;
+    firstLast[1] = lastDigit;
+    int digit;
+    digit = atoi(firstLast);
+    allSum += digit;
+    free(line);
+  }
+  printf("All Sum: %d.\n", allSum);
+  return 0;
+}
